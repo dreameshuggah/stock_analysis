3d0b804b48091d089d4f2f69cd423274f5317f12
Nor Rizal <dreameshuggah@yahoo.com>
2026-03-08 00:35:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -115,9 +115,9 @@ def metric_card(label, value, delta=None, delta_color="normal"):
     """, unsafe_allow_html=True)
 
 #st.title(":green[Financial Intelligence Dashboard]")
-st.title("Financial Intelligence Dashboard")
+#st.title("Financial Intelligence Dashboard")
 #st.markdown("###")
-#st.markdown('<p class="main-title">Financial Intelligence Dashboard</p>', unsafe_allow_html=True)
+st.markdown('<p class="main-title">Financial Intelligence Dashboard</p>', unsafe_allow_html=True)
 #st.markdown('<p class="sub-title">Premium Financial Intelligence Dashboard</p>', unsafe_allow_html=True)
 
 # Sidebar - User Input
