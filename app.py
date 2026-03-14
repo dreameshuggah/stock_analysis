3169cd05b66f8e650e7249c0ba7c6f47c8cfa1a5
Nor Rizal <dreameshuggah@yahoo.com>
2026-03-14 23:12:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ st.markdown("""
 
     .metric-value {
         color: #FFFFFF;
-        font-size: 1rem ; #1.5rem;
+        font-size: 2rem ; #1.5rem;
         font-weight: 600;
         font-family: 'Inter', sans-serif;
     }
