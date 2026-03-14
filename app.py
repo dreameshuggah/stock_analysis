d9299d6ccd0a3ffc7f4ac502a9b8a7edbf9d9752
Nor Rizal <dreameshuggah@yahoo.com>
2026-03-14 17:55:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def metric_card(label, value, delta=None, delta_color="normal"):
 #st.title("Financial Intelligence Dashboard")
 #st.markdown("###")
 st.markdown('<p class="main-title">Financial Intelligence Dashboard</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub-title">Premium Financial Analysis & Insights</p>', unsafe_allow_html=True)
+#st.markdown('<p class="sub-title">Premium Financial Analysis & Insights</p>', unsafe_allow_html=True)
 
 # Sidebar - User Input
 col1, col2 = st.columns(2)
