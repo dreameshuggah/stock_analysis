3e84db407318834ad496252cb9a34499f1a62fad
Nor Rizal <dreameshuggah@yahoo.com>
2026-03-15 15:33:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ st.markdown("""
         color: #d4af37 !important;
         font-weight: 400 !important;
         letter-spacing: 1px;
-        #text-transform: uppercase;
+        text-transform: uppercase;
         font-size: 1.2rem !important;
         margin-top: 2rem !important;
     }
